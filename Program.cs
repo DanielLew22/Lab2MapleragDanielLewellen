d6fcfd0a25c76514e04a2dbfe4519e8bda1e827d
DanielLew22 <cardinalsrule1130@gmail.com>
2022-09-15 16:51:53 -0600

    commit message

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Console.Clear();
+int A = 440;
+int B = 494;
+int C = 262;
+int D = 294;
+int E = 330;
+int F = 349;
+int G = 392;
+int wholeNote =1500 ;
+//half note? 750
+//quarter note 375
+//eighth note 187
+
+Console.Beep(E,wholeNote/4);
+Console.Beep(E,wholeNote/4);
+Console.Beep(F, wholeNote/4);
+Console.Beep(G,wholeNote/4);
+Console.Beep(G,wholeNote/4);
+Console.Beep(F,wholeNote/4);
+Console.Beep(E,wholeNote/4);
+Console.Beep(D,wholeNote/4);
+Console.Beep(C,wholeNote/4);
+Console.Beep(C,wholeNote/4);
+Console.Beep(D,wholeNote/4);
+Console.Beep(E,wholeNote/4);
+Console.Beep(E,wholeNote/4);
+Console.Beep(D,wholeNote/4);
+Console.Beep(D,wholeNote);
+Console.Beep(E,wholeNote/4);
+Console.Beep(E,wholeNote/4);
+Console.Beep(F, wholeNote/4);
+Console.Beep(G,wholeNote/4);
+Console.Beep(G,wholeNote/4);
+Console.Beep(F,wholeNote/4);
+Console.Beep(E,wholeNote/4);
+Console.Beep(D,wholeNote/4);
+Console.Beep(C,wholeNote/4);
+Console.Beep(C,wholeNote/4);
+Console.Beep(D, wholeNote/4);
+Console.Beep(E, wholeNote/4);
+Console.Beep(D, wholeNote/4);
+Console.Beep(C, wholeNote/4);
+Console.Beep(C, wholeNote);
